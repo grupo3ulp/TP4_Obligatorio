e107292b9f62e3f66920ace7db312743cb009acb
Marco Silva <nelsonmarcosilva@gmail.com>
2022-09-27 10:35:39 -0300

    agregado clase materia

---
+++ TP4_Obligatorio/src/entidades/Materia.java
@@ -0,0 +1,40 @@
+
+package tp4_obligatorio;
+
+public class Materia {
+    int idMateria;
+    String nombre;
+    int anio;
+
+    public Materia(int idMateria, String nombre, int anio) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+    
+}
