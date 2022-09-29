31f3435c1c6db102afad6458c50d93a8f576d50b
iwentothewoods <112993603+iwentothewoods@users.noreply.github.com>
2022-09-29 19:45:42 -0300

    @Override equals/hashCode

--- TP4_Obligatorio/src/entidades/Materia.java
+++ TP4_Obligatorio/src/entidades/Materia.java
@@ -41,4 +41,31 @@ public class Materia {
         return nombre + " | Año: " + anio + " | ID: " + idMateria + "\n";
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + this.idMateria;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Materia other = (Materia) obj;
+        if (this.idMateria != other.idMateria) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
 }
